67d4239b4147978db14959945e6fcfdd6d13eb10
Johns Tsai <ax_ex@hotmail.com>
2017-06-22 23:38:04 -0700

    add content and scratch of the prototype

---
+++ js/style.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @authors ${author} (${email})
+ * @date    2017-06-22
+ * @version $Id$
+ */
+
+$(document).ready(function() {
+	/* Every time the window is scrolled ... */
+	$(window).scroll( function(){
+		$('.hide').each( function(i){
+			var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+			var bottom_of_window = $(window).scrollTop() + $(window).height();
+			if( bottom_of_window > bottom_of_object ){
+				$(this).animate({'opacity':'1'},500);
+			}
+		}); 
+	});
+});
